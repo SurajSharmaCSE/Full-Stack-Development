d19544cdf018f2cb39f956e1c9320eaeb5a60a17
SurajSharmaCSE <sharmasurajkumar4649@gmail.com>
2024-03-05 23:19:46 +0530

    Practical Node js Completed

---
+++ 1. JavaScript/1.1 Node Js/3. File System Module/1. filesystem.js
@@ -0,0 +1,47 @@
+const path = require("path"); // this is path module for add dirname path 
+// ** -  fileSystem ** --
+// files -> Create,read,update,delete
+let fs = require("fs");
+
+//1 read
+let buffer = fs.readFileSync(path.join(__dirname, "abc.js"));
+console.log("bin data"+buffer) // + use for buffer conver into string
+
+//2 create
+// fs.openSync("abc2.txt","w");  // method-1
+// writeFilesyc() -> if file no exit then it will be Create and write and this methos not delete already exist content 
+fs.writeFileSync("abc3.txt","I'm hapyy today");
+
+//3 update
+fs.appendFileSync("abc.txt","Why your are happy today an reason");
+
+// ** -  Working with Folder ** --
+// files -> Create,read,update,delete
+
+//1 Create Folder
+// fs.mkdirSync("meriDirectory");
+/* Note-> path.join(__dirname => this thing only use for Create this dir in your current working folder 
+    you can use like this also 
+     fs.mkdirSync( path.join("meriDirectory"));
+*/
+
+//2 write 
+// fs.writeFileSync("meriDirectory/merifile.txt","this is mera content");
+
+//3 Read
+let content = fs.readdirSync("meriDirectory");
+console.log(content);
+
+//4 remove - use fs.unlinkSync
+for(let i=0;i<content.length;i++)
+{
+    console.log("file",content[i],"remove");
+    //remove method
+    fs.unlinkSync("meriDirectory/"+content[i]);
+}
+
+//5 remove folder
+fs.rmdirSync("meriDirectory");
+
+
+
---
+++ 1. JavaScript/1.1 Node Js/4. path Module/1. path module.js
@@ -0,0 +1,23 @@
+let path = require("path");
+let fs = require("fs");
+//1 path.join
+/* Small Quize-Create 10 folder of Lecture and include in each folder create a readme.md file 
+   and in readme.md file write somthin insize
+ */
+
+// for(let i=1;i<=3;i++)
+// {
+//     let dirPathToMake = `Lecture-${i}` ;
+//     fs.mkdirSync(dirPathToMake);
+//     fs.writeFileSync(path.join(dirPathToMake,"readme.md"),`# readme for ${dirPathToMake}`);
+// }
+
+//2 extname
+let ext =  path.extname(path.join(__dirname,"abc.js"))
+console.log("extension name:",ext);
+
+let name = path.basename(__dirname);
+console.log(name);
+
+let filename = path.basename(path.join(__dirname,"abc.js"));
+console.log(filename);
